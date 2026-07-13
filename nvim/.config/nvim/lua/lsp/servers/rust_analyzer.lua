2e379107b659f8ce47362b3c5f628c2bfbe5e494
Seokmin Woo <clematis7593@gmail.com>
2026-07-13 23:37:51 +0900

    ra

--- nvim/.config/nvim/lua/lsp/servers/rust_analyzer.lua
+++ nvim/.config/nvim/lua/lsp/servers/rust_analyzer.lua
@@ -2,14 +2,12 @@ local M = {}
 
 function M.setup(capabilities, utils)
   vim.lsp.config('rust_analyzer', utils.with_capabilities(capabilities, {
+    root_markers = {
+      { 'cargo.toml' },
+      { '.git' },
+    },
     settings = {
-      ['rust-analyzer'] = {
-        procMacro = {
-          ignored = {
-            leptos_macro = { 'server' },
-          },
-        },
-      },
+
     },
   }))
   vim.lsp.enable('rust_analyzer')
